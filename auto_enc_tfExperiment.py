8da34101443d854ac87276a5f8c9a0c0aeffe53c
Martin Frisk <yfflan@gmail.com>
2018-07-02 22:25:11 +0200

    made a tensorflow port of everything!

--- auto_enc_tfExperiment.py
+++ auto_enc_tfExperiment.py
@@ -233,7 +233,7 @@ with tf.Session() as session:
                         for x,y,r in zip(positions[0,:],positions[1,:],positions[2,:]):
 
                             radius = 10000*r
-                            transparency = 1/(r*1000)
+                            transparency = min(1, 1/(r*1000))
 
                             c = Circle((size[0]*x,size[1]*y), radius=radius, fill=False )
                             c.set_alpha(transparency)
