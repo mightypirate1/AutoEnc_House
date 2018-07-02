aa3aa5053441ecf693670d2c5f86ca68554f80c5
Martin Frisk <yfflan@gmail.com>
2018-07-02 15:21:46 +0200

    made a tensorflow port of everything!

---
+++ auto_enc_tfExperiment.py
@@ -0,0 +1,207 @@
+import pickle
+import numpy as np
+import sys
+import h5py
+import os
+import matplotlib.pyplot as plt
+import cv2
+import keras
+import tensorflow as tf
+
+'''
+Good morning!
+
+Todo:
+Make save and restore code for tf.models, when this is done. Do a training run on lab comp.
+Figure out how to get the weights out so we can initialize the robot with them.
+
+Write the damn report and send emails. Don't be too apologetic :)
+
+'''
+
+
+from lsuv_init import LSUVinit
+from spatial_autoencoder_tf import make_autoencoder
+
+MAKE_GRAYSCALE = False
+work_dir = "knut/"
+project = "pepperBig_spatialTest/"#"dev_env"
+save_every_t = 100
+display_result = not False
+visualize_convs = True
+weight_file = "weights_tf" #for outputing weights of the net in a file....
+lr = 0.00005
+minibatch_size = 1#32
+n = 1000 #numbre of data vectors per file
+n_epochs = 4100
+batch_normalization = not True
+disable_avg = False
+lsuv_init = not True
+first_batch = True
+
+def load_file(file, make_gray=False, resize=None):
+    with open(file.name,'rb') as f:
+        raw_data = pickle.load(f)
+    if make_gray:
+        print("Graying not implemented...")
+        exit()
+        raw_data = np.mean(raw_data, axis=3)
+        raw_data = raw_data.reshape(raw_data.shape+(1,))
+    if resize is not None:
+        print("Resizing not implemented yet...")
+        exit()
+        data = np.empty((n,)+size)
+        for i in range(n):
+            small = cv2.resize(raw_data[i,:,:,:3], None, fx=fx, fy=fy, interpolation=cv2.INTER_AREA) / 255.0
+            if MAKE_GRAYSCALE:
+                small = small[:,:,np.newaxis]
+            data[i,:,:,:] = small
+    else:
+        data = raw_data
+    data = data.astype(np.float32)/255.0
+    n = data.shape[0]
+    avg = np.mean(data,axis=0)[np.newaxis,:]
+    if batch_normalization or disable_avg:
+        avg *= 0.0
+    avg_block = np.concatenate((avg,)*n,axis=0)
+    return data, n, avg, avg_block
+
+############
+############
+############
+############
+
+
+files = sys.argv[2:]
+n_files = len(sys.argv[2:])
+mode = sys.argv[1]
+assert mode in ["--train", "--test"], "Invalid mode..."
+training = True if mode=="--train" else False
+testing = not training
+file_idx = 0
+
+size = (96,96,3)
+
+# size = (28,28,3)
+# from tensorflow.examples.tutorials.mnist import input_data
+# mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+# mnist = mnist.train.images.reshape((-1,28,28))[:1000,:,:,np.newaxis]
+# mnist = np.concatenate((mnist,mnist,mnist), axis=3)
+# avg = np.mean(mnist, axis=0)[np.newaxis,:,:,:]
+# avg_block = np.concatenate((avg,)*1000,axis=0)
+
+with tf.Session() as session:
+    # keras.backend.set_session(session)
+    input_tf = tf.placeholder(shape=(None,)+size, dtype=tf.float32)
+    output_tf, snoop_tf, position_tf, train_mode_tf = make_autoencoder(input_tf,size=size,lr=lr,bn=batch_normalization, sess=session)
+    loss_tf = tf.losses.mean_squared_error(output_tf, input_tf)
+    training_ops = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss_tf)
+    saver = tf.train.Saver()
+    train_writer = tf.summary.FileWriter( './logs/1/train ', session.graph)
+    init_ops = tf.global_variables_initializer()
+
+    if training:
+        session.run(init_ops)
+        # tf.summary.histogram('loss', loss_tf)
+        # tf.summary.image('snoop', snoop_tf[0,:,:,:3])
+
+        T=-1
+        total_samples = 0
+        for t in range(n_epochs):
+            for infile in os.listdir(work_dir+project+"/data"):
+                T+=1
+                with open(work_dir+project+"/data/"+infile,'rb') as file:
+                    data, n, avg, avg_block = load_file(file)
+                idx=0
+                tot_loss = 0
+                n = 10
+                merge = tf.summary.merge_all()
+                print("[",end='',flush=True)
+                while idx<n-1:
+                    feed_dict={
+                                input_tf : (data-avg_block)[idx:min(n,idx+minibatch_size),:,:,:],
+                                train_mode_tf : True,
+                               }
+                    _,snoop,loss = session.run([training_ops, snoop_tf, loss_tf], feed_dict=feed_dict)
+                    tot_loss += minibatch_size * loss
+                    idx += minibatch_size
+                    print("-",end='',flush=True)
+                print("]",flush=True)
+                # train_writer.add_summary(summary, T)
+                total_samples += n
+                print("t={} -> n={} | loss={}".format(T,total_samples,tot_loss/n) )
+
+                if T%save_every_t == 0:
+                    path = work_dir+project+"/nets_tf/AE_tf_{}".format(10000+T)
+                    print("Saving net ({})...".format(path),end='',flush=True)
+                    save_path = saver.save(session, path)
+                    print("[x]")
+
+    print("ADD CODE FOR WEIGHT-EXPORT")
+
+    if testing:
+        idx = 0
+        print("Layers loaded:")
+        for net in files:
+            print("net: {}".format(net))
+            saver.restore(session,net)
+            names = session.graph.get_tensor_by_name('training:0')
+            tensor_names = tf.trainable_variables()
+            weights = session.run(tensor_names, feed_dict=None)
+            layer_names = [x.name.split("/")[0] for x in tensor_names]
+            weight_dict={}
+            for i,x in enumerate(layer_names):
+                weight_dict[x] = []
+            for i,x in enumerate(layer_names):
+                weight_dict[x].append( weights[i].shape )
+            for x in weight_dict:
+                print(x, weight_dict[x])
+            file_output = list(weight_dict.values())
+            print("Saving weights to {}".format(weight_file+"{}.pkl".format(idx)))
+            with open(weight_file+".pkl",'wb') as out_file:
+                pickle.dump(file_output, out_file, pickle.HIGHEST_PROTOCOL)
+
+    ''' Compare inputs and outputs '''
+    if testing:
+        print("Comparing input w. output...")
+        for net in files:
+            print(net)
+            saver.restore(session,net)
+            for infile in os.listdir(work_dir+project+"/data"):
+                with open(work_dir+project+"/data/"+infile,'rb') as file:
+                    data, n, avg, _ = load_file(file)
+                for i in range(n):
+                    feed_dict={
+                                input_tf : (data-avg)[i,:,:,:][np.newaxis,:,:,:],
+                                train_mode_tf : False,
+                               }
+                    output,snoop, positions, loss = session.run([output_tf, snoop_tf, position_tf, loss_tf], feed_dict=feed_dict)
+                    org = data[i,:,:,:]
+                    clone = (output+avg)[0]
+                    snoop_layers = snoop[0]
+                    positions = positions[0]
+                    e = loss #np.sqrt(np.sum(np.square(np.abs(org-clone)),axis=2)).reshape(-1)
+                    print("Mean error: {}    Max error: {}".format(e.mean(),e.max()))
+                    if display_result:
+                        for j in range(positions.shape[1]):
+                            print("Feature {} pos: ({},{})".format(j,positions[0,j], positions[1,j]))
+                        snoop_destack_tuple = (org,clone)
+                        if snoop_layers.shape[-1]%3 != 0:
+                            snoop_layers = np.concatenate((snoop_layers, np.zeros( (size[0], size[1],3-snoop_layers.shape[-1]%3)) ), axis=-1)
+                        limit = int(snoop_layers.shape[-1]/3)
+                        snoop_destack_tuple += tuple([ (0.2**(i%2))+(-1)**(i%2)*snoop_layers[:,:,3*i:3*(i+1)] for i in range(limit)])
+                        n = len(snoop_destack_tuple)
+                        h = min(4,int(np.sqrt(n)))
+                        if n%h != 0:
+                            snoop_destack_tuple += (np.zeros( (size[0], size[1],3) ) )*(h-n%h)
+                            n+=h-n%h
+                        img = np.concatenate(snoop_destack_tuple[0:int(n/h)],axis=1)
+                        idx = int(n/h)
+                        for i in range(1,h):
+                            x = np.concatenate( snoop_destack_tuple[idx:min(idx+int(n/h),len(snoop_destack_tuple))],axis=1)
+                            img = np.concatenate(  (img,x) , axis=0 )
+                            idx += int(n/4)
+                        if not visualize_convs:
+                            img = np.concatenate( (org,clone), axis=1 )
+                        plt.imshow(img)
+                        plt.show()
---
+++ spatial_autoencoder_tf.py
@@ -0,0 +1,169 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Model
+from keras.optimizers import Adam,SGD
+from keras.layers import Input, Lambda, Convolution2D, MaxPooling2D, Dropout, Flatten, Dense, Concatenate, UpSampling2D, BatchNormalization, Activation
+from keras import backend as K
+import numpy as np
+import tensorflow as tf
+
+USE_POOLING = True
+
+def spatial_soft_argmax(z,size, alpha=1.0):
+    pos_x, pos_y = space_blocks(size)
+    max_z = tf.reduce_max(z , axis=1, keep_dims=True)
+    max_z = tf.reduce_max(max_z , axis=2, keep_dims=True)
+    tmp = z-max_z
+    exp_z = tf.exp(alpha*z)
+    weights = tf.reduce_max(exp_z, axis=1, keep_dims=True)
+    weights = tf.reduce_max(weights, axis=2, keep_dims=True)
+    softmax = tf.truediv(exp_z, weights)
+    map_x = pos_x * softmax
+    map_y = pos_y * softmax
+    x = tf.reduce_sum( tf.reduce_sum(map_x, axis=1, keep_dims=True), axis=2, keep_dims=False )
+    y = tf.reduce_sum( tf.reduce_sum(map_y, axis=1, keep_dims=True), axis=2, keep_dims=False )
+    return tf.concat([x,y], axis=1)
+
+def position_decoder(z,size):
+    ''' Takes a tensor z of shape (samples, 2 , c) where
+        the 2nd dimention refers to x,y coordintates. I.e. x,y coordintates for
+        c number of features. Returns feature maps of desired size
+        (but the number of channels need still be c, so size=(i,j,c)), where the
+        point x,y gets value close to 1, and further away less and less.
+        '''
+    pos_x, pos_y = space_blocks(size)
+    x = tf.reshape(z[:,0,:], (-1,1,1,size[2]))
+    y = tf.reshape(z[:,1,:], (-1,1,1,size[2]))
+    x_coords = tf.multiply(x, tf.ones(size))
+    y_coords = tf.multiply(y, tf.ones(size))
+    pos_x, pos_y = space_blocks(size)
+    delta_x_squared = tf.square(pos_x-x_coords)
+    delta_y_squared = tf.square(pos_y-y_coords)
+    dist_map = tf.sqrt(delta_x_squared+delta_y_squared)
+    return dist_map
+
+def space_blocks(size):
+    a = np.tile( (np.arange(size[0], dtype=np.float32)/size[0]).reshape((size[0],1,1)), (1,size[1],size[2]) )
+    b = np.tile( (np.arange(size[1], dtype=np.float32)/size[1]).reshape((1,size[1],1)), (size[0],1,size[2]) )
+    return tf.convert_to_tensor(a),tf.convert_to_tensor(b)
+
+def max_abs_error(y_true, y_pred):
+    return K.max(K.abs( y_true - y_pred ))
+
+def custom_loss(y_true, y_pred):
+    a = 0.1
+    b = 1.0
+    return a*max_abs_error(y_true, y_pred) + b*keras.losses.mean_absolute_error(y_true, y_pred)
+
+def make_autoencoder(input_tensor, size,lr=0.02,bn=False, sess=None):
+    allow_bias = True
+    if sess is not None:
+        keras.backend.set_session(sess)
+    initializer = tf.contrib.layers.xavier_initializer_conv2d(uniform=True)
+
+    conv_depth_1 = 32
+    conv_depth_2 = 32
+    conv_depth_3 = 64
+
+    size_1 = (5,5)#(8,8)
+    size_2 = (5,5)
+    size_3 = (5,5)
+    stride_1 = 1
+    stride_2 = 1
+    stride_3 = 1
+
+    training = tf.placeholder(dtype=tf.bool, name='training')
+
+    x = tf.layers.conv2d(
+                     input_tensor,
+                     conv_depth_1,
+                     name='conv1',
+                     padding='same',
+                     kernel_size=size_1,
+                     strides=stride_1,
+                     activation=tf.nn.elu,
+                     kernel_initializer=initializer,
+                     bias_initializer=initializer
+                    )
+    x = tf.layers.dropout(x, rate=0.2, training=training)
+
+    x = tf.layers.conv2d(
+                     x,
+                     conv_depth_2,
+                     name='conv2',
+                     padding='same',
+                     kernel_size=size_2,
+                     strides=stride_2,
+                     activation=tf.nn.elu,
+                     kernel_initializer=initializer,
+                     bias_initializer=initializer
+                    )
+    x = tf.layers.dropout(x, rate=0.2, training=training)
+
+    x = tf.layers.conv2d(
+                     x,
+                     conv_depth_3,
+                     name='conv3',
+                     padding='same',
+                     kernel_size=size_3,
+                     strides=stride_3,
+                     activation=tf.nn.elu,
+                     kernel_initializer=initializer,
+                     bias_initializer=initializer
+                    )
+    x = tf.layers.dropout(x, rate=0.2, training=training)
+
+
+    ''' He we smuggle out some information... '''
+    snoop = x
+    encoded = spatial_soft_argmax(x,(size[0],size[1],conv_depth_3))
+    positions = encoded
+    ''' Decoder starts here... '''
+    x = position_decoder(encoded,(size[0],size[1],conv_depth_3))
+    ''' ------------------------------------- '''
+
+    x = tf.layers.dropout(x, rate=0.2, training=training)
+
+
+    x = tf.layers.conv2d(
+                     x,
+                     conv_depth_3,
+                     name='deconv1',
+                     padding='same',
+                     kernel_size=size_3,
+                     strides=stride_3,
+                     activation=tf.nn.elu,
+                     kernel_initializer=initializer,
+                     bias_initializer=initializer
+                    )
+    x = tf.layers.dropout(x, rate=0.2, training=training)
+
+    x = tf.layers.conv2d(
+                     x,
+                     conv_depth_2,
+                     name='deconv2',
+                     padding='same',
+                     kernel_size=size_2,
+                     strides=stride_2,
+                     activation=tf.nn.elu,
+                     kernel_initializer=initializer,
+                     bias_initializer=initializer
+                    )
+    x = tf.layers.dropout(x, rate=0.2, training=training)
+
+    x = tf.layers.conv2d(
+                     x,
+                     size[2],
+                     name='deconv3',
+                     padding='same',
+                     kernel_size=size_1,
+                     strides=stride_1,
+                     activation=tf.nn.elu,
+                     kernel_initializer=initializer,
+                     bias_initializer=initializer
+                    )
+    x = tf.layers.dropout(x, rate=0.2, training=training)
+
+    output = x
+
+    return output, snoop, positions, training
