f83bc3ecf020f1341e0933eb7687a6b64e7f0eb3
Martin Frisk <yfflan@gmail.com>
2018-07-03 03:10:04 +0200

    made a tensorflow port of everything!

--- spatial_autoencoder_tf.py
+++ spatial_autoencoder_tf.py
@@ -125,8 +125,8 @@ def make_autoencoder(input_tensor, size, alpha=1.0, lr=0.02,bn=False, sess=None,
     ''' Decoder starts here... '''
     if use_dense_decoder:
         x = tf.layers.dense(encoded, 256, activation=tf.nn.elu, kernel_initializer=initializer, bias_initializer=initializer)
-        x = tf.layers.dense(x, (size[0]//2)*(size[1]//2)*size[2], activation=tf.nn.sigmoid, kernel_initializer=initializer, bias_initializer=initializer)
-        x = tf.reshape(x, (-1, size[0]//2, size[1]//2,size[2]))
+        x = tf.layers.dense(x, size[0]*size[1]*size[2], activation=tf.nn.sigmoid, kernel_initializer=initializer, bias_initializer=initializer)
+        x = tf.reshape(x, (-1, size[0], size[1],size[2]))
         output = tf.image.resize_nearest_neighbor(x, (size[0],size[1]))
         return output, snoop, positions, alpha_tf, training
     else:
