5fa94d216379ab819c2e5c3d6cb8b8e8f2b151c8
Martin Frisk <yfflan@gmail.com>
2018-07-30 18:54:59 +0200

    fixed so that alpha is also saved to the weights

--- auto_enc.py
+++ auto_enc.py
@@ -109,7 +109,7 @@ def save_weights_to_file(file_path):
     for i,x in enumerate(layer_names):
         weight_dict[x[0]] = { 'layer' : x[0], 'weights' : {} }
     for i,x in enumerate(layer_names):
-        if 'batch' in x[0] or 'conv' in x[0]:
+        if 'batch' in x[0] or 'conv' in x[0] or 'spatial_soft_argmax' in x[0]:
             weight_dict[x[0]]['weights'][ x[1].split(":")[0] ] = weights[i]
     file_output = list(weight_dict.values())
     for x in file_output:
